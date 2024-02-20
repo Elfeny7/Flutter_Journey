52876ddc722e186b899e94954d0c4b3b3c1e2f76
elfeny7 <ikmalfaris50@gmail.com>
2024-02-20 21:40:37 +0700

    finishing consume api with dio + bloc

---
+++ dio_try/lib/pages/bloc/user_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:dio_try/data/datasource/remote_datasource.dart';
+import 'package:flutter/material.dart';
+import 'package:dio_try/data/model/user.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'user_event.dart';
+part 'user_state.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final RemoteDataSource remoteDataSource;
+  UserBloc({required this.remoteDataSource}) : super(UserInitial()) {
+    on<LoadUser>((event, emit) async {
+      emit(UserLoading());
+      try {
+        final result = await remoteDataSource.getUsers();
+        emit(UserLoaded(result.data));
+      } catch (error) {
+        emit(UserError(error.toString()));
+      }
+    });
+  }
+}
